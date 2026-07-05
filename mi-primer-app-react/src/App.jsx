870e4d60d0260c96ec668e4ac96e4b0867893dc2
Daiana Martin <martindaiana165@gmail.com>
2026-07-05 01:06:40 -0300

    Agrego useState

--- mi-primer-app-react/src/App.jsx
+++ mi-primer-app-react/src/App.jsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
+
 function App() {
-  return (
-    <h1>Hola Mundo desde React - P3</h1>
-  );
+  const [mensaje] = useState('Hola Mundo desde React - P3');
+
+  return <h1>{mensaje}</h1>;
 }
 
 export default App;
